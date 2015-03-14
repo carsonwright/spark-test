597906cf5314d1a1b2d9f3fd3526e833c586c169
Carson Wright <carsonnwright@gmail.com>
2015-03-14 16:06:41 -0400

    connection pool

--- src/main/java/com/traitify/HelloWorld.java
+++ src/main/java/com/traitify/HelloWorld.java
@@ -1,15 +1,21 @@
 package com.traitify;
+
+import com.mchange.v2.c3p0.DataSources;
 import org.javalite.activejdbc.Base;
+
+import javax.sql.DataSource;
+
+import java.sql.SQLException;
 
 import static spark.Spark.get;
 
 public class HelloWorld {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws SQLException {
+        DataSource dataSourceUnpooled = DataSources.unpooledDataSource("jdbc:postgresql://localhost:5432/awesome", "carson", "mypasseasy");
+        DataSource dataSourcePooled = DataSources.pooledDataSource(dataSourceUnpooled); //init the connection pool
         get("/hello", (req, res) -> {
-          Base.open("org.postgresql.Driver",
-                "jdbc:postgresql://localhost:5432/awesome",
-                "carson",
-                "mypasseasy");
+          Base.open(dataSourcePooled); //get connection from pool
           String response = "Hello World There Are Now " + Employee.findAll() + " Users";
           Base.close();
           return response;
